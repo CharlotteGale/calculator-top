008a52d2e0a80da0c91681ac1524a88e62c2eeeb
Charlotte Gale <CharlotteMGale@outlook.com>
2024-12-20 18:06:16 +0000

    Add logic and basic html
    add simple functions for base calculations to script.js
         add, subtract, multiply & divide
    add simple layout for calculator in index.html

---
+++ src/script.js
@@ -0,0 +1,16 @@
+// addition function
+function addition(num1, num2) {
+     return num1 + num2;
+};
+// subtraction function
+function subtraction(num1, num2) {
+     return num1 - num2;
+};
+// multiplication function
+function multiplication(num1, num2) {
+     return num1 * num2;
+};
+// division function
+function division(num1, num2) {
+     return num1 / num2;
+};
